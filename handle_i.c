40434167469a2a164b125c8fd394a3bb2fe7e14e
kelvinthuo999 <thuokelvinngigi@gmail.com>
2023-06-27 06:14:55 -0700

    printing integers

---
+++ handle_i.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * printf_int - prints integer
+ * @args: argument to print
+ * Return: number of characters printed
+ */
+int printf_int(va_list args)
+{
+	int n = va_arg(args, int);
+	int num = n; /* Store the absolute value of n */
+	int digit, exp = 1; /* Variables for digit extraction and place value */
+	int is_negative = 0; /* Flag to track if the number is negative */
+	int i; /* Counter for characters printed */
+
+	if (n < 0)
+	{
+		is_negative = 1;
+		num = -num; /* Make num positive for digit extraction */
+	}
+
+	/* Calculate the place value of the leftmost digit */
+	while (num / 10 != 0)
+	{
+		exp *= 10;
+		num /= 10;
+	}
+
+	i = is_negative + 1; /* Increment i to account for negative sign */
+	if (is_negative)
+		_putchar('-'); /* Print '-' if the number is negative */
+
+	num = n;
+	while (exp > 0)
+	{
+		digit = num / exp; /* Extract the leftmost digit */
+		_putchar(digit + '0'); /* Convert digit to character and print */
+		num %= exp; /* Remove the leftmost digit from num */
+		exp /= 10; /* Decrement the place value */
+		i++; /* Increment character count */
+	}
+
+	return (i); /* Return the number of characters printed */
+}
